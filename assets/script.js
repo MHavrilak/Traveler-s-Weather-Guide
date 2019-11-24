31221ac122a4577eaaf4db8b6e6c723e429f78f8
Hava-hizzle <mikehavrilak@gmail.com>
2019-11-24 12:15:19 -0500

    new updates

--- assets/script.js
+++ assets/script.js
@@ -1,54 +1,36 @@
-// Function for pulling weather data
-$(document).ready(function(){ 
-
- 
-
-var city = $("#main");
+var weather = "Havrilak";
 var date = "";
-var icon = ""
-var temp = ""
-var humidty = ""
-var winSpd = ""
-var uvIndx = ""
+var icon = "";
+var temp = "";
+var humidty = "";
+var winSpd = "";
+var uvIndx = "";
+var apiKey = "4383960b162385ee11decc2446137670";
+
+$( document ).ready(function() {
+  console.log( "ready!" );
+  getWeatherURL()
+});
 
 // Weather API for current weather
-$(document).ready(function) {
-
-  function weather() {
-    var queryURL = "api.openweathermap.org/data/2.5/forecast?id=524901" + weather + "&apikey=4383960b162385ee11decc2446137670";
-
-    $.get.JSON(URL, function(data) {
-        console.log(data);
-        updateDOM("data");
-    }
-  }
-}
-  weather();
+function getWeatherURL() {
+  var queryURL = "api.openweathermap.org/data/2.5/forecast?q= " + search + '&units=imperial&appid=' + apiKey;
+  console.log(queryURL);
   
-  function updateDOM () {
-      $("city").html(city);
-  }
-}
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-        console.log(response);
-      // Create and save a reference to new empty table row
-    var newRow = $("<tr>");
-    
-      // Create and save references to elements containing weather info from the AJAX response object
-    var temp = $("<li>").text(response.temp);
-    var humidty = $("<li>").text(response.humidity);
-    var winSpd = $("<li>").text(response.wind);
-    var uvIndx = $("<li>").text(response.UV);
-      // Append the td eleents to the new table row
-     newRow.append(Temp);
-     newRow.append(Humidity);
-     newRow.append(Wind);
-     newRow.append(UVIndex);
-      // Append the table row to the tbody element
-    $("tbody").append(newRow);
-    });
-  }
-    
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+    crossDomain: true
+  }).then(function(r){
+    console.log(r.data.d);
+  })
+};
+
+
+// function updateDOM () {
+    // $("city").html(city);
+// }
+
+
+
+  
