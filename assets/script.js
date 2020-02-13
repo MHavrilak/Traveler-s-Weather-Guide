08f802869ac933c000a485b11a74f0b3dc0dcab8
Hava-hizzle <mikehavrilak@gmail.com>
2020-02-12 22:37:08 -0500

    html and script updates

--- assets/script.js
+++ assets/script.js
@@ -7,37 +7,63 @@ var winSpd = "";
 var uvIndx = "";
 var apiKey = "4383960b162385ee11decc2446137670";
 
-$( document ).ready(function() {
-  console.log( "ready!" );
+$(document).ready(function () {
+  console.log("ready!");
   getWeatherURL()
 });
 
 // Weather API for current weather
 function getWeatherURL() {
-  searchtext.value="baltimore";
+  searchtext.value = "baltimore";
   var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q= " + searchtext.value + '&units=imperial&appid=' + apiKey;
   console.log(queryURL);
-  
+  var currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q= " + searchtext.value + "&units=imperial&appid=" + apiKey;
+  console.log(currentQueryURL);
+  var uvQuery = "http://api.openweathermap.org/data/2.5/uvi?"  + searchtext.value + "&appid={appid}&lat={lat}&lon={lon}" + apiKey;
+  console.log(uvQuery);
   $.ajax({
     url: queryURL,
     method: "GET",
     crossDomain: true
-  }).then(function(r){
+  }).then(function (r) {
     console.log(r.list.length);
     console.log(r);
 
-    for (var i=0; i < r.list.length; i++) {
-      
+    for (var i = 0; i < 5; i++) {
+      var weatherCard = $(".weathercard")[i];
+      var data = r.list[i];
+      var fiveDayTemp = data.main.temp;
+      $(weatherCard).find(".temp").text(fiveDayTemp);
+      $(weatherCard).find(".humidity").text(fiveDayTemp);
     }
 
   })
+  $.ajax({
+    url: currentQueryURL,
+    method: "GET",
+    crossDomain: true
+  }).then(function (r) {
+    console.log(r);
+
+    $("#temp").text(r.main.temp);
+    $("#humidity").text(r.main.humidity);
+    $("#windspeed").text(r.wind.speed);
+
+  })
+  $.ajax({
+    url: uvQueryURL,
+    method: "GET",
+    crossDomain: true
+  }).then(function (r) {
+    console.log(r.list.length);
+    console.log(r);
+
+    $("#uvindex").text(r)
+    })
+
 };
 
 
-// function updateDOM () {
-    // $("city").html(city);
-// }
 
 
 
-  
